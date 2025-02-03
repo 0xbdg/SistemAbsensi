08ea563f4e5a603717f0f64ade9db4c09f4acf89
?0 <86694445+0xbdg@users.noreply.github.com>
2025-02-03 23:24:58 +0700

    Update tugasakhir.ino

--- tugasakhir/tugasakhir.ino
+++ tugasakhir/tugasakhir.ino
@@ -3,55 +3,56 @@
 #include <SPI.h>
 #include <MFRC522v2.h>
 #include <MFRC522DriverSPI.h>
-//#include <MFRC522DriverI2C.h>
 #include <MFRC522DriverPinSimple.h>
 #include <MFRC522Debug.h>
 #include <WiFiManager.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
 #include <HTTPClient.h>
-// Learn more about using SPI/I2C or check the pin assigment for your board: https://github.com/OSSLibraries/Arduino_MFRC522v2#pin-layout
+#include <WiFiUdp.h>
+#include <NTPClient.h>               
+
 MFRC522DriverPinSimple ss_pin(4);
 
-MFRC522DriverSPI driver{ss_pin}; // Create SPI driver
-//MFRC522DriverI2C driver{};     // Create I2C driver
-MFRC522 mfrc522{driver};         // Create MFRC522 instance
+MFRC522DriverSPI driver{ss_pin}; 
+
+MFRC522 mfrc522{driver};        
 
 MFRC522::MIFARE_Key key;
 
-byte blockAddr = 2;
-byte newBlockData[17] = {"JohnDoe"};
-byte len = 18;
-byte blockDataRead[18];
-
 const String sheet_url = "SHEET_URL";
+const String api_endpoint_url = "ENDPOINT_URL";
 
 const int rs = 27, en = 26, d4 = 32, d5 = 25, d6 = 13, d7 = 14;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 #define BUZZER_PIN 5
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 25200, 60000);
+byte last_second, second_, minute_, hour_, day_, month_;
+char Time[ ] = "TIME:00:00:00";
+char Date[ ] = "DATE:00/00/2000";
+
 void setup() {
   lcd.begin(16,2);
   SPI.begin();
   WiFi.mode(WIFI_STA);
   Serial.begin(9600); 
-
-  pinMode(BUZZER_PIN, OUTPUT);
+  
   WiFiManager wm;
 
   wm.autoConnect("Absensi", "password");
-
+  pinMode(BUZZER_PIN, OUTPUT);
   lcd.setCursor(0, 0);
-  lcd.print("Tap kartu anda");
+  lcd.print(" Tap kartu anda");
 
   while (!Serial);       // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
   
   mfrc522.PCD_Init();    // Init MFRC522 board.
-  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
-
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
+  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);  // Show details of PCD - MFRC522 Card Reader details.
+  
+  timeClient.begin();
 }
 
 void loop() {
@@ -60,102 +61,125 @@ void loop() {
   }
 
   if (!mfrc522.PICC_ReadCardSerial()) {
-	return;
+        return;
   }
 
-  lcd.setCursor(0, 0);
-  lcd.print("Tap kartu anda");
   
   Serial.print("Card UID: ");
   MFRC522Debug::PrintUID(Serial, (mfrc522.uid));
   Serial.println();
 
-  //writeDataFromCard(blockAddr, newBlockData);
-  readDataFromCard(blockAddr, blockDataRead);
-
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
+  String uid = getUID();
+  Serial.println(uid);
+  verifyData(uid);
+  delay(1000);
+  
 }
 
-void readDataFromCard(byte blockAddress, byte readBlockData[]){
+String getDate(){
+  timeClient.update();
+  unsigned long epochTime = timeClient.getEpochTime(); 
+  
+  int days = epochTime / 86400L;
+  int year = 1970 + (days / 365);
+  days %= 365;
+  int month = 1 + (days / 30);
+  int day = days % 30 + 1;
 
-  if (mfrc522.PCD_Authenticate(0x60, blockAddress, &key, &(mfrc522.uid)) != 0) {
-    Serial.println("Authentication failed.");
-    return;
-  }
-
-  if (mfrc522.MIFARE_Read(blockAddress, readBlockData, &len) != 0) {
-    Serial.println("Read failed.");
-  } else {
-    String data = "";
-    Serial.println("Read successfully!");
-    Serial.print("Data in block ");
-    Serial.print(blockAddress);
-    Serial.print(": ");
-    for (byte i = 0; i < 16; i++) {
-      Serial.print(String((char)blockDataRead[i]));  // Print as character
-      data += String((char)blockDataRead[i]);
-    }
-    Serial.println();
-
-    if (data == "" && data.length() <= 0){
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Kartu belum");
-      lcd.setCursor(0, 1);
-      lcd.print("terdaftar!!");
-      tone(BUZZER_PIN, 1500);
-      delay(500);
-      tone(BUZZER_PIN, 1500);
-      delay(500);
-      noTone(BUZZER_PIN);
-      delay(2000);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tap kartu anda");
-    }
-    else{
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("nama:");
-      lcd.setCursor(0, 1);
-      lcd.print(data);
-      tone(BUZZER_PIN, 3000);
-      delay(1000);
-      noTone(BUZZER_PIN);
-      sendData(data);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tap kartu anda");
-    }
-  }
+  return String(day) +"/"+ String(month)+"/"+String(year);
 }
 
-void writeDataFromCard(byte blockAddress, byte writeBlockData[]){
+String getTime(){
+  timeClient.update();
+  unsigned long epochTime = timeClient.getEpochTime(); 
+  
+  int currentHour = (epochTime % 86400L) / 3600;  // Extract hour
+  int currentMinute = (epochTime % 3600) / 60;    // Extract minute
+  int currentSecond = epochTime % 60;              // Extract second
 
-  if (mfrc522.PCD_Authenticate(0x60, blockAddress, &key, &(mfrc522.uid)) != 0) {
-    Serial.println("Authentication failed.");
-    return;
-  }
-  if (mfrc522.MIFARE_Write(blockAddress, writeBlockData, 16) != 0) {
-    Serial.println("Write failed.");
-  } else {
-    Serial.print("Data written successfully in block: ");
-    Serial.println(blockAddress); 
-  }
+  return String(currentHour) +":"+ String(currentMinute)+":"+String(currentSecond);
 }
 
-void sendData(String data){
+String getUID(){
+  String uidString = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    if (mfrc522.uid.uidByte[i] < 0x10) {
+      uidString += "0"; 
+    }
+    uidString += String(mfrc522.uid.uidByte[i], HEX);
+  }
+  return uidString;
+}
+
+void verifyData(String uid){
   if (WiFi.status() == WL_CONNECTED){
     HTTPClient http;
+    http.begin(api_endpoint_url + uid);
 
-    http.begin(sheet_url + String(data));
+    int httpCode = http.GET();
+
+    if (httpCode > 0){
+       Serial.printf("%d\n", httpCode);
+
+       String response = http.getString();
+
+       DynamicJsonDocument doc(1024);
+       DeserializationError error = deserializeJson(doc, response);
+      
+       if (error) {
+         lcd.clear();
+         lcd.setCursor(0,0);
+         lcd.print("Kartu belum");
+         lcd.setCursor(0,1);
+         lcd.print("terdaftar!!!");
+         delay(2000);
+         lcd.clear();
+         lcd.setCursor(0,0);
+         lcd.print(" Tap kartu anda");
+         Serial.print("Failed to parse JSON: ");
+         Serial.println(error.f_str());
+         return;
+       }
+       else{
+         lcd.clear();
+         lcd.setCursor(0,0);
+         lcd.print("DATE:"+getDate());
+         lcd.setCursor(0,1);
+         lcd.print("TIME:"+getTime());
+         String nama = doc["nama"];
+         String kelas = doc["kelas"];
+         String jurusan = doc["jurusan"];
+
+         sendDataToSpreadsheet(nama, kelas, jurusan);
+
+
+         delay(2000);
+         lcd.clear();
+         lcd.setCursor(0,0);
+         lcd.print(" Tap kartu anda");
+       }
+    }
+    else {
+       Serial.println(httpCode);
+    }
+
+    http.end();
+  }
+}
+
+void sendDataToSpreadsheet(String nama, String kelas, String jurusan){
+  if (WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    String payload = "?nama="+nama+"&kelas="+kelas+"&jurusan="+jurusan;
+
+    http.begin(sheet_url + payload);
 
     int status_code = http.GET();
 
     if (status_code > 0){
       Serial.println("data terkirim");
     }
+    
     else{
       Serial.printf("Error dengan status: %d\n",status_code);
     }
